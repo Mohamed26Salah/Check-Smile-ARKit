e18dde7319bb10c7664cf5a40bea997e08651a36
Mohamed26Salah <mohamedsalahomran44@gmail.com>
2024-09-11 15:04:53 +0300

    init

---
+++ CheckSmile/CheckSmile/CheckSmileApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSmileApp.swift
+//  CheckSmile
+//
+//  Created by Mohamed Salah on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CheckSmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
